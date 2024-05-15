2ed799ced0e43b18bd8526b32fa28f7f640b0e8e
Rlopeze <mumolopeze@uc.cl>
2024-05-15 12:21:01 -0400

    load .env correctly

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,25 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'changeme')
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
   filter(
     None,
-    os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(','),
+    os.getenv('DJANGO_ALLOWED_HOSTS', '').split(','),
   )
 )
 
