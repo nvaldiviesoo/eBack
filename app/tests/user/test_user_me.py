2639ed797df0c90ed85e83d9efd6b3a587ff96b3
Pedro de Pablo <pedro.depabloi@uc.cl>
2024-05-26 19:51:17 -0400

    add user/me tests

---
+++ app/tests/user/test_user_me.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, RequestFactory
+from rest_framework.test import force_authenticate
+from core.models import User
+from user.views import UserModelViewSet
+
+class UserMeTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@example.com', name='Test User')
+
+    def test_me(self):
+        request = self.factory.get('/user/me')
+        force_authenticate(request, user=self.user)
+        view = UserModelViewSet.as_view({'get': 'me'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['data']['email'], self.user.email)
