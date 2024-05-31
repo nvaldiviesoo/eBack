c46ffc9b879be04b24a0d21678988eb047f4234a
Pedro de Pablo <pedro.depabloi@uc.cl>
2024-05-31 16:23:46 -0400

    test(user/me): Add failing test

--- app/tests/user/test_user_me.py
+++ app/tests/user/test_user_me.py
@@ -8,10 +8,18 @@ class UserMeTest(TestCase):
         self.factory = RequestFactory()
         self.user = User.objects.create(email='test@example.com', name='Test User')
 
-    def test_me(self):
+    def test_get_me_valid_token(self):
         request = self.factory.get('/user/me')
         force_authenticate(request, user=self.user)
         view = UserModelViewSet.as_view({'get': 'me'})
         response = view(request)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['data']['email'], self.user.email)
+    
+    def test_me_no_token(self):
+        request = self.factory.get('/user/me')
+        force_authenticate(request, user=None)
+        view = UserModelViewSet.as_view({'get': 'me'})
+        response = view(request)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data['error'], 'Not authenticated')
