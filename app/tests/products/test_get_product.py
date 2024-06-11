f393009451d680d3a153a937e83ce487ce319101
1862507J <andresbravod@uc.cl>
2024-06-11 10:29:24 -0400

    Feat: Creado Test para show_Products_by_name

---
+++ app/tests/products/test_get_product.py
@@ -0,0 +1,73 @@
+from django.test import TestCase, RequestFactory
+from rest_framework.test import force_authenticate, APIClient
+from core.models import User, Product
+from product.views import ProductViewSet
+import json
+
+class Product_show_by_name(TestCase): 
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@example.com', name='Test User', is_staff=True)
+        self.quantity_1 = 10
+        self.quantity_2 = 9
+        self.quantity_3 = 8
+        self.product1 = Product.objects.create(name='Test Product',
+                                              price=1000, quantity=self.quantity_1, description='Test Description', size='M', user=self.user, category='Shorts',
+                                              color='Red')
+        self.product2 = Product.objects.create(name='Test Product',
+                                              price=1000, quantity=self.quantity_2, description='Test Description', size='S', user=self.user, category='Shorts',
+                                              color='Red')
+        self.product3 = Product.objects.create(name='Test Product',
+                                              price=1000, quantity=self.quantity_3, description='Test Description', size='S', user=self.user, category='Shorts',
+                                              color='Blue')
+        self.apiclient = APIClient()
+        self.url = '/api/v1/products/get_product_by_name/'
+        self.apiclient.force_authenticate(user=self.user)
+    
+    def test_get_product_by_name(self):
+        
+        data = {'name': 'Test Product'}
+        response = self.apiclient.get(self.url, data)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['data']['name'], 'Test Product')
+        self.assertEqual(response.data["quantity"]["Red"], {"S": self.quantity_2, "M": self.quantity_1})
+        self.assertEqual(response.data["quantity"]["Blue"], {"S": self.quantity_3})
+    
+    def test_get_product_by_name_not_found(self):
+        
+        data = {'name': 'Test Product 2'}
+        response = self.apiclient.get(self.url, data)
+        
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['error'], 'Product not found')
+    
+    def test_get_product_by_name_no_name(self):
+            
+            response = self.apiclient.get(self.url)
+            
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(response.data['error'], 'Product not found')
+    
+    def test_get_product_by_name_agregate_item(self):
+        data = {'name': 'Test Product'}
+        response = self.apiclient.get(self.url, data)
+        
+        # Estos assert son solo para verificar que el endpoint funciona correctamente de antemano
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['data']['name'], 'Test Product')
+        self.assertEqual(response.data["quantity"]["Red"], {"S": self.quantity_2, "M": self.quantity_1})
+        self.assertEqual(response.data["quantity"]["Blue"], {"S": self.quantity_3})
+        
+        #Probamos crear un nuevo producto para saber que el endpoint se actualiza correctamente
+        new_product = Product.objects.create(name='Test Product',
+                                              price=1000, quantity=1, description='Test Description', size='XL', user=self.user, category='Shorts',
+                                              color='Blue')
+        response = self.apiclient.get(self.url, data)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['data']['name'], 'Test Product')
+        self.assertEqual(response.data["quantity"]["Red"], {"S": self.quantity_2, "M": self.quantity_1})
+        self.assertEqual(response.data["quantity"]["Blue"], {"S": self.quantity_3, "XL": 1})
+        
+        
